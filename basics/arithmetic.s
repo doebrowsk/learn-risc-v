e6cf8fcc2caa47cc18069c336e16f4816fc8098e
doebrowsk <11599974+doebrowsk@users.noreply.github.com>
2020-12-30 17:19:24 -0500

    immediates and byte loading

--- basics/arithmetic.s
+++ basics/arithmetic.s
@@ -18,6 +18,7 @@ add x20, x21, x8
 ld x8, 64(x22)
 # since the doubleword at index 8 would be 64 bytes into the array's memory space
 
+
 # Example 2.3.3
 # A[12] = h + A[8]
 # h is x21 and A[base_addr: x22]
@@ -25,3 +26,20 @@ ld x8, 64(x22)
 add x9, x8, x21
 sd x9, 96(x22)
 # could have added back in to x8 e.g. add x8, x8, x21
+
+
+# constants can be written directly in assembly
+addi x9, x9, 4 # x9 = x9 + 4
+# addi can use negative numbers as well (the immediate is a 2s-complement)
+
+# register x0 holds the constant value 0
+# can be used to negate the value in a register using subtraction
+sub x9, x0, x9 # negates the value of x9 
+
+
+# loading doublewords from memory into a register with the same size (64bits) makes signed / unsigned destinction moot
+# we only care about a difference for signed / unsigned when loading individual bytes
+lb t1, 0(s1) # signed byte load - performs proper sign extension when filling the target register with the byte value
+lbu t1, 0(s1) # unsigned byte load - just fills the left-side bytes in the target register with 0
+# loading bytes sees usage primarily for loading text characters
+# lbu sees the most usage as a result (we want to load the exact bit values with no sign consideration
